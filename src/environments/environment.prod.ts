6e0a78cb35d80d9cb0e9a6a2985514753e8cfd17
Andrepeduti <andre.peduti@hotmail.com>
2025-12-30 05:27:31 -0300

    feat ajuste QRcode

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
     production: true,
     useMocks: false,
     apiUrl: 'https://api.iloversapp.com.br/api/v1',
-    mercadoPagoPublicKey: 'APP_USR-b8118204-5cde-4072-b8d8-5611b22303ee' // TODO: Replace with Production Key
+    mercadoPagoPublicKey: 'APP_USR-b8118204-5cde-4072-b8d8-5611b22303ee'
 };
